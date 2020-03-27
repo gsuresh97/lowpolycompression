93825c07e32e2d6fccbcc4bc62e74c4cb3a50df4
Gopi Suresh <suresh.gopikrishnan@gmail.com>
2020-03-26 23:18:50 -0700

    Finished Coloring Poly art

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # lowpolycompression
 This is an investigative work that sees if converting all the frames in a video into low poly frames improves compression in native H.264. An extension to this will create a custom encoding algorithm that compresses low poly frames and benchmarks that against the native H.264 compression.
+
+To generate low poly art pictures run:
+```
+python3 gen_vid.py wave.jpg 15000
+```
+
+Replace the first argument with the file name and the second with the number of vertices you want there to be in the image.
--- poly_img.py
+++ poly_img.py
@@ -10,6 +10,7 @@ class PolyImg:
     self.max_points = max_points
     self.rate = rate
     self.orig = cv.imread(path)
+    self.low_poly_img = np.ones((self.orig.shape[0], self.orig.shape[1], 3))*255
 
   def _preprocess(self, img):
     # Convert image to greyscale
@@ -28,28 +29,90 @@ class PolyImg:
 
   def _get_nodes(self, img):
     nodes_convolution = signal.convolve2d(img, np.ones((3, 3))/9)
-    sparse_nodes = nodes_convolution > 0.1
+    sparse_nodes = nodes_convolution > 0.3
     nodes = np.transpose(sparse_nodes.nonzero())
-    selected_idx = np.random.randint(len(nodes), size=self.max_points)
-    return nodes[selected_idx, :]
+    n = sparse_nodes.nonzero()
+    # nodes.T[[0, 1]] = nodes.T[[1, 0]]
+    if self.max_points > len(nodes):
+      selected_idx = np.random.choice(len(nodes), 
+                                      size=len(nodes), 
+                                      replace=False)
+    else:
+      selected_idx = np.random.choice(len(nodes), 
+                                      size=self.max_points,
+                                      replace=False)
+    # img_dimensions = self.orig.shape
+    nodes = nodes[selected_idx, :]
+    return nodes
 
   def _create_triangles(self, nodes):
     tri = Delaunay(nodes)
     return tri
 
+  def _draw_poly_lines(self, tri):
+    points = tri.points
+    simplices = tri.simplices
+    all_poly = []
+    for simplex in simplices:
+      tri_points = []
+      for vertex in simplex:
+        tri_points.append(points[vertex])
+      all_poly.append(tri_points)
+    cv.polylines(self.low_poly_img, 
+                np.array(all_poly, dtype=np.int32), 
+                True,
+                (0, 0, 1))
+
+  def _draw_poly_points(self, tri):
+    points = tri.points
+    for point in points:
+      cv.circle(self.low_poly_img, 
+                (int(point[0]), int(point[1])),
+                1, 
+                [0, 0, 1],
+                -1)
+  
+  def _draw_poly_img(self, tri):
+    points = np.array(tri.points)
+    simplices = tri.simplices
+    for simplex in simplices:
+      tri_points = []
+      for vertex in simplex:
+        # print(points[vertex])
+        tri_points.append([int(points[vertex][1]), int(points[vertex][0])])
+      centroid = (
+        int((tri_points[0][1] + tri_points[1][1] + tri_points[2][1])/3),
+        int((tri_points[0][0] + tri_points[1][0] + tri_points[2][0])/3)
+      )
+      if centroid[0] >= self.low_poly_img.shape[0]:
+        centroid = (self.low_poly_img.shape[0]-1, centroid[1])
+      if centroid[1] >= self.low_poly_img.shape[1]:
+        centroid = (centroid[0], self.low_poly_img.shape[1]-1)
+      
+      color = tuple([x/255 for x in self.orig[centroid]])
+      # print(f'Original color: {self.orig[centroid]}, Color Value: {color}')
+      # print(tri_points)
+      cv.fillPoly(self.low_poly_img, 
+                  np.array([tri_points], dtype=np.int32), 
+                  color)
+      # break
+
   def plot_nodes(self):
     pp_img = self._preprocess(self.orig)
     nodes = self._get_nodes(pp_img)
     triangles = self._create_triangles(nodes)
-    # plt.scatter(nodes[0], nodes[1])
+    plt.scatter(nodes[0], nodes[1])
     plt.triplot(nodes[:,0], nodes[:,1], triangles.simplices.copy())
     plt.plot(nodes[:,0], nodes[:,1], 'o')
     plt.show()
-  
+
   def get_low_poly(self):
     pp_img = self._preprocess(self.orig)
-    # nodes = self._get_nodes(pp_img)
-    return pp_img
+    nodes = self._get_nodes(pp_img)
+    triangles = self._create_triangles(nodes)
+    self._draw_poly_img(triangles)
+    return self.low_poly_img
+    # return pp_img
   
   def show_low_poly(self):
     cv.imshow("Low Poly image", self.get_low_poly())
